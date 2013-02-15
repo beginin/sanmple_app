92557df0343bf8b44bd59e8da7fd303ea6c06063
begininva <begininva@it-a304.flagman.int>
2013-02-15 14:19:47 +0800

    Add Pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SanmpleApp::Application.config.secret_token = 'd6c26c7724b50d1dbc528dd4fd0eae8a64b0751a30ccb244c33dbf83d0d0ac87bb4a186ebc516337d465f75f36f9b0bb510d6d7d1ffaed13d29a7505c5651527'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SanmpleApp::Application.config.session_store :cookie_store, key: '_sanmple_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SanmpleApp::Application.config.session_store :active_record_store
